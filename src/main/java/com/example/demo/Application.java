673faf4154fc5bdd9b68ab3cc23508889038c11e
Balkrishna <balkrishnabhojagude@gmail.com>
2021-05-22 15:42:16 +0530

    Anil done changes for story 100

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+	private static int i=10;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	} 
