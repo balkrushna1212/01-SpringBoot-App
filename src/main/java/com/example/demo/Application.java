c3441a105f920e47c6289bbc1b83a2e2f35bba01
Balkrishna <balkrishnabhojagude@gmail.com>
2021-05-22 14:26:57 +0530

    doProcess() added

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -9,5 +9,9 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+        public void doProcess(){
+             //b.logic added .
+           int i=10;
+        }
 
 }
