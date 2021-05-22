aaf354bad9eeba886e28d5a5e94f3aaec3e92ae4
Balkrishna <balkrishnabhojagude@gmail.com>
2021-05-22 14:46:35 +0530

    HIS-300 m1() added

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -8,10 +8,13 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
+	} 
+        //HIS-300
         public void doProcess(){
              //b.logic added .
            int i=10;
         }
-
+        public void m1(){
+            //b.logic added .
+        }
 }
